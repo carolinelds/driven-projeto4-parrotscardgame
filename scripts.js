faed21b67fc4d72ac85a092dd81ba8ec8c43f05c
carolinelds <carolinelds@gmail.com>
2022-02-06 02:32:37 -0300

    Prompt the user for number of cards and check if valid

--- scripts.js
+++ scripts.js
@@ -5,4 +5,21 @@ if (window.matchMedia("(max-width: 614px)").matches) {
    let checkMobile = false;
 }
 
+// global constants
+const backgroundGifs = [1, 2, 3, 4, 5, 6, 7];
+const validQtyCards = [4, 6, 8, 10, 12, 14];
 
+beginGame(); // call first prompt
+
+function beginGame(){
+    
+    let qtyCards = prompt("Com quantas cartas deseja jogar? (números pares de 4 até 14)");
+
+    qtyCards = parseInt(qtyCards);
+
+    if (validQtyCards.includes(qtyCards)) {
+        sortCards(qtyCards);
+    } else {
+        begin();
+    }
+}
