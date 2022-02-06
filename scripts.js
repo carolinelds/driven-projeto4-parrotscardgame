b58c41bd8a02a3196b63ad2bb05f68ce00fc0503
carolinelds <carolinelds@gmail.com>
2022-02-06 04:50:49 -0300

    Turn cards when user clicks (no game rules applied yet)

--- scripts.js
+++ scripts.js
@@ -1,13 +1,20 @@
-//check if layout is mobile or desktop
+/*//check if layout is mobile or desktop
 if (window.matchMedia("(max-width: 614px)").matches) {
     let checkMobile = true;
 } else {
    let checkMobile = false;
-}
+}*/
 
 // global constants
 const backgroundGifs = [1, 2, 3, 4, 5, 6, 7];
 const validQtyCards = [4, 6, 8, 10, 12, 14];
+
+// global variable
+let cards = {
+    backImg: [],
+    posFirst: [],
+    posSecond: []
+};
 
 beginGame(); // call first prompt
 
@@ -51,22 +58,22 @@ function sortCards(number){
         posSecondCard[i] = positions.lastIndexOf(i+1);
     }
 
-    // create cards object
-    const cards = {
+    // update cards object
+    cards = {
         backImg: backImg,
         posFirst: posFirstCard,
         posSecond: posSecondCard
     }
     
     // position cards in their respective first and second positions
-    positionCards(number,cards,positions);
+    positionCards(number,positions);
 }
 
 function comparator() { 
 	return Math.random() - 0.5; 
 }
 
-function positionCards(number,object,order){
+function positionCards(number,order){
 
     const firstRow = document.querySelector(".first-row");
     const secondRow = document.querySelector(".second-row");
@@ -74,20 +81,38 @@ function positionCards(number,object,order){
     for (let i = 0; i < number; i++){
 
         if (i <= number/2 - 1){ //position first half in .first-row
-           firstRow.innerHTML += `
-            <div class="card">
-                <img src="./images/front.png">
-                <img src="./images/${order[i]}.gif" class="display-none">
+            firstRow.innerHTML += `
+            <div class="card" onclick="turnCard(this)" data-identifier="card">
+                <div class="front-face face" data-identifier="front-face">
+                    <img src="./images/front.png">
+                </div>
+                <div class="back-face face" data-identifier="back-face">
+                    <img src="./images/${order[i]}.gif">
+                </div>             
             </div>
             `;
 
         } else { //position second half in .second-row
             secondRow.innerHTML += `
-            <div class="card">
-                <img src="./images/front.png">
-                <img src="./images/${order[i]}.gif" class="display-none">
+            <div class="card" onclick="turnCard(this)" data-identifier="card">
+                <div class="front-face face" data-identifier="front-face">
+                    <img src="./images/front.png">
+                </div>
+                <div class="back-face face" data-identifier="back-face">
+                    <img src="./images/${order[i]}.gif">
+                </div>             
             </div>
             `;
         }
     }
+}
+
+function turnCard(item){  
+        
+    const back = item.querySelector(".back-face");
+    back.classList.toggle("active");
+
+    const front = item.querySelector(".front-face");
+    front.classList.toggle("active");
+
 }
